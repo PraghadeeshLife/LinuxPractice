2cb366a515b333dfb90d874fa2fb0d0196612a7f
Praghadeesh T K S <praghadeeshthevendria@gmail.com>
2022-10-07 13:15:17 +0530

    Added GCD and LCM

---
+++ CPP/LCM/lcm.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	int n1, n2, hcf, lcm;
+
+	cout << "Enter two numbers: ";
+	cin >> n1 >> n2;
+
+	for(int i=1; i<=n1 && i<=n2; ++i){
+		if(n1%i == 0 && n2%i == 0){
+			hcf = i;
+		}
+	}
+
+	lcm = (n1*n2)/hcf;
+
+	cout << "LCM = " << lcm;
+	return 0;
+
+}
+
